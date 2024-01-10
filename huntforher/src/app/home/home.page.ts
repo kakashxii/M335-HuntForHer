ef389e283fc43ed7556902047a968a38c0ded8eb
kakashxii <sabrinaayla.seckanovic@gmail.com>
2024-01-10 13:44:22 +0100

    - added functionality to home page button -fixed background intro page

--- huntforher/src/app/home/home.page.ts
+++ huntforher/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private  router: Router) {}
+
+  //navigate to intro pae
+  goToIntroPage(){
+    this.router.navigate( ['/intro']).then(
+      () => {
+        console.log('Navigation to Introduction Page successful');
+      },
+      (error) => {
+        console.error('Navigation to Introduction Page failed')
+      }
+    );
+  }
 
 }
