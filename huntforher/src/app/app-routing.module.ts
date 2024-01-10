98c237d6cd1a8b50f0fd8dfd34521038b427d129
kakashxii <sabrinaayla.seckanovic@gmail.com>
2024-01-10 14:23:31 +0100

    fixed merge conflict

--- huntforher/src/app/app-routing.module.ts
+++ huntforher/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 887c80b87f444c65766b5437eb868e878591e94b
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
@@ -17,66 +13,24 @@ const routes: Routes = [
   },
   {
     path: 'intro',
-    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
-<<<<<<< HEAD
+    loadChildren: () => import('./intro/intro.module').then(m => m.IntroPageModule)
   },
   {
-  path: 'playing-rules',
-  loadChildren: () => import('./playing-rules/playing-rules.module').then( m => m.PlayingRulesPageModule)
-},
-
-  {
-=======
-  },{
->>>>>>> 887c80b87f444c65766b5437eb868e878591e94b
-    path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
-  },
-  {
-    path: 'past-hunts',
-    loadChildren: () => import('./past-hunts/past-hunts.module').then( m => m.PastHuntsPageModule)
-  },  {
-<<<<<<< HEAD
-    path: 'getting-started',
-    loadChildren: () => import('./getting-started/getting-started.module').then( m => m.GettingStartedPageModule)
-=======
     path: 'playing-rules',
-    loadChildren: () => import('./playing-rules/playing-rules.module').then( m => m.PlayingRulesPageModule)
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./playing-rules/playing-rules.module').then(m => m.PlayingRulesPageModule)
   },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'intro',
-    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
-  },
-  {
-  path: 'playing-rules',
-  loadChildren: () => import('./playing-rules/playing-rules.module').then( m => m.PlayingRulesPageModule)
-},
-
   {
     path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule)
   },
   {
     path: 'past-hunts',
-    loadChildren: () => import('./past-hunts/past-hunts.module').then( m => m.PastHuntsPageModule)
->>>>>>> 4c243c2791ba1c07f0ef02ae4f54819df4e152e6
->>>>>>> 887c80b87f444c65766b5437eb868e878591e94b
+    loadChildren: () => import('./past-hunts/past-hunts.module').then(m => m.PastHuntsPageModule)
   },
-
-
+  {
+    path: 'getting-started',
+    loadChildren: () => import('./getting-started/getting-started.module').then(m => m.GettingStartedPageModule)
+  }
 ];
 
 @NgModule({
