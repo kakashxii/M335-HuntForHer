daa98f385fb6c42bc98f039c32a6e82abe5d9b0c
kakashxii <sabrinaayla.seckanovic@gmail.com>
2024-01-10 14:52:07 +0100

    fixed button scss

--- huntforher/src/app/playing-rules/playing-rules.page.ts
+++ huntforher/src/app/playing-rules/playing-rules.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-playing-rules',
   templateUrl: './playing-rules.page.html',
   styleUrls: ['./playing-rules.page.scss'],
 })
-export class PlayingRulesPage implements OnInit {
+export class PlayingRulesPage {
 
-  constructor() { }
+  constructor(private  router: Router) {}
 
-  ngOnInit() {
+  startHunting() {
+    this.router.navigate(['/getting-started']).then(
+      () => {
+        console.log('Navigation to Introduction Page successful');
+      },
+
+      (error) => {
+        console.error('Navigation to Introduction Page failed')
+      }
+    );
   }
 
 }
