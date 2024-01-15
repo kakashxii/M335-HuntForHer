f8969400d2640780be6cd3b4ecbce79f47bf465a
kakashxii <sabrinaayla.seckanovic@gmail.com>
2024-01-15 22:31:44 +0100

    modified past-hunts

--- huntforher/src/app/past-hunts/past-hunts.page.ts
+++ huntforher/src/app/past-hunts/past-hunts.page.ts
@@ -21,6 +21,11 @@ export class PastHuntsPage implements OnInit {
     if (allPastHuntsJson) {
       this.pastHunts = JSON.parse(allPastHuntsJson);
       console.log('All Past Hunts:', this.pastHunts); // Log to check if data is retrieved
+
+      // Iterate through past hunts and load rewards
+      this.pastHunts.forEach((pastHunt) => {
+        pastHunt.rewards = this.getTotalCollectedRewards(pastHunt);
+      });
     }
   }
 
