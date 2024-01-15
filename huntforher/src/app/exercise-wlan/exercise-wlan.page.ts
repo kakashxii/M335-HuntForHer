a75f1e62d727ebcb92a57d68e8911cbf7aa85d8b
patrick <patrick.bettler@icloud.com>
2024-01-15 14:46:32 +0100

    wlan timer zum immer wieder überprüfen ob verbunden

--- huntforher/src/app/exercise-wlan/exercise-wlan.page.ts
+++ huntforher/src/app/exercise-wlan/exercise-wlan.page.ts
@@ -1,6 +1,6 @@
 import { Network, ConnectionStatus } from '@capacitor/network';
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-exercise-wlan',
@@ -10,11 +10,22 @@ import {Router} from "@angular/router";
 export class ExerciseWlanPage implements OnInit {
   isWifiConnected: boolean = false;
   isNextButtonEnabled: boolean = false;
+  private wifiCheckInterval: any;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.startNetworkListener();
+
+    // Überprüfe alle 5 Sekunden, ob das Gerät mit dem WLAN verbunden ist
+    this.wifiCheckInterval = setInterval(() => {
+      this.checkWifiConnection();
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    // Wenn die Seite verlassen wird, stoppe die Überprüfung
+    clearInterval(this.wifiCheckInterval);
   }
 
   async startNetworkListener() {
@@ -35,8 +46,16 @@ export class ExerciseWlanPage implements OnInit {
     this.isNextButtonEnabled = this.isWifiConnected;
   }
 
+  checkWifiConnection() {
+    // Überprüfe alle 5 Sekunden die WLAN-Verbindung
+    const status = Network.getStatus().then((status: ConnectionStatus) => {
+      this.updateNetworkStatus(status);
+    });
+  }
 
   doneButton() {
+    // Stoppe die Überprüfung, wenn die Seite verlassen wird
+    clearInterval(this.wifiCheckInterval);
     this.router.navigate(['/tabs/load-exercise']);
   }
 }
